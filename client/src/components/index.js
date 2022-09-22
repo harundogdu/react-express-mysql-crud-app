bc7cf50eb14d246a77db9817ff7d9d5a07d1c5ef
Harun Doğdu <harundogdu06@gmail.com>
2022-09-22 23:27:33 +0300

    fix(client) : client made it better

---
+++ client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as BookItem } from './book-item';
+export { default as CreateBook } from './create-book';
---
+++ client/src/constants/index.js
@@ -0,0 +1,5 @@
+const CONSTANTS = {
+  API_URL: process.env.REACT_APP_API_URL
+};
+
+export default CONSTANTS;
